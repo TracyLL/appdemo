cd5ac33f8b19537746902360413af4ea50fbdab3
TracyLL <llm89624@sina.com>
2014-11-26 11:15:34 +0800

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :users
+ 
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
